ae0f55e509f6a8549d82bb0197e3c964429d0b35
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2020-11-20 19:48:49 -0500

    Fixed relative url for 404 webms.

--- javascripts/404.js
+++ javascripts/404.js
@@ -7,5 +7,5 @@ webm[0] = "walama.webm";
 webm[1] = "MAGA.webm";
 
 var num = Math.floor(Math.random() * webm.length);
-var thewebm = "webm/" + webm[num];
+var thewebm = "/webm/" + webm[num];
 document.getElementById('mywebm').src = thewebm;
