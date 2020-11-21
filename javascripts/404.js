1507c6e5156b896c8812300c29db341b726b6f5a
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2020-11-20 19:42:39 -0500

    Changed how 404 works

---
+++ javascripts/404.js
@@ -0,0 +1,11 @@
+/*!
+ * pick random webm
+ */
+
+var webm = new Array();
+webm[0] = "walama.webm";
+webm[1] = "MAGA.webm";
+
+var num = Math.floor(Math.random() * webm.length);
+var thewebm = "webm/" + webm[num];
+document.getElementById('mywebm').src = thewebm;
