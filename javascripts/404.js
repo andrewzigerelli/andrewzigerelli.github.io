6ae48a0cff7ebd2a6fd06276ae5914474a388a3c
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2021-01-15 21:36:26 -0500

    resized

--- javascripts/404.js
+++ javascripts/404.js
@@ -4,8 +4,9 @@
 
 var webm = new Array();
 webm[0] = "walama.webm";
-webm[1] = "MAGA.webm";
-webm[2] = "mccain.webm";
+webm[1] = "mccainout.webm";
+webm[2] = "MAGA.webm";
+webm[3] = "mccain.webm";
 
 var num = Math.floor(Math.random() * webm.length);
 var thewebm = "\/webm/" + webm[num];
